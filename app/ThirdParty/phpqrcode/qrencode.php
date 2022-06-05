2b7527ce96d2c8a3fed7d99859c746704dfa368f
¨EduardoHG27¨ <eduardoenrique.hernandez@gmail.com>
2022-06-04 23:21:28 -0500

    elevenk commit

--- app/ThirdParty/phpqrcode/qrencode.php
+++ app/ThirdParty/phpqrcode/qrencode.php
@@ -282,6 +282,7 @@
         //----------------------------------------------------------------------
         public static function png($text, $outfile = false, $level = QR_ECLEVEL_L, $size = 3, $margin = 4, $saveandprint=false) 
         {
+            var_dump("qr function");
             $enc = QRencode::factory($level, $size, $margin);
             return $enc->encodePNG($text, $outfile, $saveandprint=false);
         }
