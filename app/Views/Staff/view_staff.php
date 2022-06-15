0a0c819cc8ce41f30d97235d4954fce07829a42d
¨EduardoHG27¨ <eduardoenrique.hernandez@gmail.com>
2022-06-14 22:19:06 -0500

    Update

--- app/Views/Staff/view_staff.php
+++ app/Views/Staff/view_staff.php
@@ -1984,18 +1984,7 @@
 
 
     $("#btn_copiar_horario").click(function() {
-        const {
-            value: email
-        } = await Swal.fire({
-            title: 'Input email address',
-            input: 'email',
-            inputLabel: 'Your email address',
-            inputPlaceholder: 'Enter your email address'
-        })
-
-        if (email) {
-            Swal.fire(`Entered email: ${email}`)
-        }
+      
     });
 
 
